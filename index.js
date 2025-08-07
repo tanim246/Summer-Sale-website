3c8c93b183a3e2f216051cf5d9e07afea3f8e955
Tanim <mdtasnimhossaintanim@gmail.com>
2025-08-07 22:31:54 +0600

    full completw my code

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const nameList = document.getElementById("nameList");
-let total = document.getElementById("total")
+let total = document.getElementById("total");
 
-const discountTotal = document.getElementById("discountTotal")
+const discountTotal = document.getElementById("discountTotal");
+const purchaseBtn = document.getElementById("purchaseBtn");
+const applyBtn = document.getElementById("applyBtn");
+const inputCupon = document.getElementById("inputCupon");
+const discountTk = document.getElementById("discount");
+
 
 let totalPrice = 0;
 const handleClick = (data) => {
@@ -12,10 +17,42 @@ const handleClick = (data) => {
   const amountNumber = parseInt(amount);
 
   totalPrice = totalPrice + amountNumber;
-  total.innerText=totalPrice
-  discountTotal.innerText = totalPrice
-//   console.log(totalPrice);
+  total.innerText = totalPrice;
+  discountTotal.innerText = totalPrice;
+  //   console.log(totalPrice);
   const li = document.createElement("li");
   li.innerText = name;
   nameList.appendChild(li);
+  if (totalPrice > 0) {
+    purchaseBtn.disabled = false;
+  }
+  if (totalPrice > 200) {
+    applyBtn.disabled = false;
+  }
 };
+
+applyBtn.addEventListener("click", () => {
+  if (inputCupon.value.trim() === "") {
+    alert("Input kali kne");
+    return;
+  }
+
+  else if (inputCupon.value === "SELL200") {
+    const discount = totalPrice * 0.2;
+    const totalWithDiscount = totalPrice - discount;
+    discountTk.innerText = discount;
+    discountTotal.innerText = totalWithDiscount;
+  } else {
+    alert("Coupon dosn't match");
+  }
+});
+
+purchaseBtn.addEventListener("click", () => {
+  totalPrice = 0;
+  // console.log(totalPrice)
+
+  total.innerText = " 00";
+  discountTotal.innerText = "00";
+  discountTk.innerText = "00";
+  nameList.innerHTML = "";
+});
